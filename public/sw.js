9499fce3ef676cf32fb19b2c757c2c17173bebbe
Tyson Skiba <tyson@skiba.com.au>
2026-08-01 21:42:32 +1000

    Add: PWA

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'v1_cache'
+const ASSETS_TO_CACHE = ['/', '/index.html']
+
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			return cache.addAll(ASSETS_TO_CACHE)
+		}),
+	)
+})
+
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			return response || fetch(event.request)
+		}),
+	)
+})
--- src/components/Rounds.tsx
+++ src/components/Rounds.tsx
@@ -93,7 +93,6 @@ export const Rounds: React.FC<RoundProps> = ({ game, draftOwned, setGame }) => {
 											onClick={() => setOverlay(assignment?.character!)}
 										/>
 									)}
-									{/* TODO: Objectives here */}
 								</div>
 							)
 						})}
