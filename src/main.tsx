e28b7979a4cb96bfee7e6706546c54812a964754
Tyson Skiba <tyson@skiba.com.au>
2026-08-01 21:54:20 +1000

    Fix: Use vite plguin for pwa

--- src/main.tsx
+++ src/main.tsx
@@ -3,29 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 
-if ('serviceWorker' in navigator && window.location.hostname !== 'localhost') {
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/sw.js').then((registration) => {
-			registration.update()
-
-			registration.onupdatefound = () => {
-				const installingWorker = registration.installing
-				if (installingWorker) {
-					installingWorker.onstatechange = () => {
-						if (
-							installingWorker.state === 'installed' &&
-							navigator.serviceWorker.controller
-						) {
-							console.log('New content available; reloading page...')
-							window.location.reload()
-						}
-					}
-				}
-			}
-		})
-	})
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,35 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'Villainous Tournament Round Randomiser',
+				short_name: 'Villainous Tournament',
+				theme_color: '#E7B76A',
+				background_color: '#111016',
+				icons: [
+					{
+						src: 'favicon.ico',
+						sizes: '64x64 32x32 24x24 16x16',
+						type: 'image/x-icon',
+					},
+					{
+						src: 'logo192.png',
+						type: 'image/png',
+						sizes: '192x192',
+					},
+					{
+						src: 'logo512.png',
+						type: 'image/png',
+						sizes: '512x512',
+					},
+				],
+			},
+		}),
+	],
 })
